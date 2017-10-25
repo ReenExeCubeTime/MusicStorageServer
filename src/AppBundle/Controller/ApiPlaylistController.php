92d00e55d6b5e83cdb3b1ad012f709f18dbe671f
ReenExe <ipourri@gmail.com>
2017-10-26 01:30:30 +0300

    use autowire #4

--- src/AppBundle/Controller/ApiPlaylistController.php
+++ src/AppBundle/Controller/ApiPlaylistController.php
@@ -3,13 +3,27 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Playlist;
+use Doctrine\Bundle\DoctrineBundle\Registry;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
-class ApiPlaylistController extends Controller
+class ApiPlaylistController
 {
+    /**
+     * @var Registry
+     */
+    private $doctrine;
+
+    /**
+     * ApiPlaylistController constructor.
+     * @param Registry $doctrine
+     */
+    public function __construct(Registry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
     /**
      * @Route("/api/v1/playlist", name="api.playlist.default", methods={"GET"})
      */
@@ -25,11 +39,10 @@ class ApiPlaylistController extends Controller
     {
         $title = $request->request->get('title');
 
-        $manager = $this->getDoctrine()->getManager();
-
         $playlist = new Playlist();
         $playlist->setTitle($title);
 
+        $manager = $this->doctrine->getManager();
         $manager->persist($playlist);
         $manager->flush($playlist);
 
